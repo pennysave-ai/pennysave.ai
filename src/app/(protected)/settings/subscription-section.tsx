cf747b3c76c29ba5009099ffcf34ebf14ac73ae0
Mike <mike.zhylevych@gmail.com>
2025-01-27 21:08:23 +0100

    added protocl to websocket

--- src/app/(protected)/settings/subscription-section.tsx
+++ src/app/(protected)/settings/subscription-section.tsx
@@ -59,14 +59,15 @@ export default function SubscriptionSection() {
                     {user.subscription?.cancelAt
                       ? "will be canceled on"
                       : "renews on"}{" "}
-                    {format(
-                      new Date(
-                        user.subscription?.cancelAt ||
-                          user.subscription?.expires ||
-                          ""
-                      ),
-                      "PP"
-                    )}
+                    {user.subscription?.cancelAt ||
+                      (user.subscription?.expires &&
+                        format(
+                          new Date(
+                            user.subscription?.cancelAt ||
+                              user.subscription?.expires
+                          ),
+                          "PP"
+                        ))}
                   </div>
                 </div>
               </div>
