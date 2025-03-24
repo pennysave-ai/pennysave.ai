c50480be7101d0d64d9bb44edf3d993de57ea19c
Mike <mike.zhylevych@gmail.com>
2025-03-24 22:37:34 +0100

    security upgrade

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { headers } from "next/headers";
-
-export async function getCurrentRoute() {
-  const headersList = headers();
-  const pathname = headersList.get("x-current-path") || "";
-  return { pathname };
-}
