2f3d2a5e033143ea8d58f3c4cff1031a4b964a0b
Mike <mike.zhylevych@gmail.com>
2025-03-27 23:28:16 +0100

    improvements

--- src/app/(protected)/settings/banks-section.tsx
+++ src/app/(protected)/settings/banks-section.tsx
@@ -118,7 +118,7 @@ export default function BanksSection({
                   </Chip>
                 </div>
                 <StripeLink
-                  className="z-0 group relative inline-flex items-center justify-center box-border appearance-none select-none whitespace-nowrap font-normal subpixel-antialiased overflow-hidden tap-highlight-transparent data-[pressed=true]:scale-[0.97] outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 px-4 min-w-20 h-10 text-small gap-2 rounded-medium [&>svg]:max-w-[theme(spacing.8)] transition-transform-colors-opacity motion-reduce:transition-none bg-primary text-primary-foreground data-[hover=true]:opacity-hover w-full md:w-auto"
+                  className="w-auto hidden md:flex"
                   title="Connect"
                   startContent={
                     <Icon icon="solar:add-circle-bold" width={20} />
