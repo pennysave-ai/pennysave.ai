12c537897f5e2f172535db7687a889b5d11c9d3c
Mike <mike.zhylevych@gmail.com>
2025-04-06 20:00:38 +0200

    redundant elements

--- src/app/(protected)/dashboard/ai-helper.tsx
+++ src/app/(protected)/dashboard/ai-helper.tsx
@@ -1,64 +1,64 @@
-import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useChat } from "ai/react";
+// import { useState, useEffect } from "react";
+// import { useSession } from "next-auth/react";
+// import { useChat } from "ai/react";
 
 import { Icon } from "@iconify/react";
-import { useModal } from "@/app/providers/modal";
+// import { useModal } from "@/app/providers/modal";
 import { Card, CardBody, CardHeader } from "@heroui/card";
 import { Button } from "@heroui/button";
 import { Divider } from "@heroui/divider";
 import { ScrollShadow } from "@heroui/scroll-shadow";
-import PromptArea from "./prompt-area";
-import Conversation from "./conversation";
-import { USER_INPUT_LIMIT } from "@/constants";
+// import PromptArea from "./prompt-area";
+// import Conversation from "./conversation";
+// import { USER_INPUT_LIMIT } from "@/constants";
 
 interface AIHelperProps {
   onClose: () => void;
 }
 
 export default function AIHelper({ onClose }: AIHelperProps) {
-  const [responseStarted, setResponseStarted] = useState(false);
-  const [currentTool, setCurrentTool] = useState<string | null>(null);
-  const { data } = useSession();
-  const { onOpen: onPaywallModalOpen } = useModal();
-  const handleChatScroll = () => {
-    const chat = document.querySelector("#chat-wrapper");
-    chat?.scrollTo({
-      top: chat.scrollHeight,
-      behavior: "smooth",
-    });
-  };
-  const { messages, isLoading, input, handleSubmit, setInput, error } = useChat(
-    {
-      onResponse: () => {
-        setResponseStarted(true);
-      },
-      onFinish: () => {
-        setResponseStarted(false);
-        setCurrentTool(null);
-        handleChatScroll();
-      },
-      onToolCall: ({ toolCall }) => {
-        setCurrentTool(toolCall.toolName);
-      },
-    }
-  );
+  // const [responseStarted, setResponseStarted] = useState(false);
+  // const [currentTool, setCurrentTool] = useState<string | null>(null);
+  // const { data } = useSession();
+  // const { onOpen: onPaywallModalOpen } = useModal();
+  // const handleChatScroll = () => {
+  //   const chat = document.querySelector("#chat-wrapper");
+  //   chat?.scrollTo({
+  //     top: chat.scrollHeight,
+  //     behavior: "smooth",
+  //   });
+  // };
+  // const { messages, isLoading, input, handleSubmit, setInput, error } = useChat(
+  //   {
+  //     onResponse: () => {
+  //       setResponseStarted(true);
+  //     },
+  //     onFinish: () => {
+  //       setResponseStarted(false);
+  //       setCurrentTool(null);
+  //       handleChatScroll();
+  //     },
+  //     onToolCall: ({ toolCall }) => {
+  //       setCurrentTool(toolCall.toolName);
+  //     },
+  //   }
+  // );
 
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (data?.user.hasActiveStripeSubscription) {
-      handleSubmit(e);
-      setResponseStarted(false);
-    } else {
-      onPaywallModalOpen();
-    }
-  };
+  // const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   if (data?.user.hasActiveStripeSubscription) {
+  //     handleSubmit(e);
+  //     setResponseStarted(false);
+  //   } else {
+  //     onPaywallModalOpen();
+  //   }
+  // };
 
-  useEffect(() => {
-    if (isLoading) {
-      handleChatScroll();
-    }
-  }, [isLoading]);
+  // useEffect(() => {
+  //   if (isLoading) {
+  //     handleChatScroll();
+  //   }
+  // }, [isLoading]);
 
   return (
     <Card>
@@ -89,17 +89,17 @@ export default function AIHelper({ onClose }: AIHelperProps) {
             className="flex h-[30vh] flex-col gap-6 overflow-y-auto pb-8"
             id="chat-wrapper"
           >
-            <Conversation
+            {/* <Conversation
               messages={messages}
               isLoading={isLoading && !responseStarted}
               responseStarted={responseStarted}
               isTyping={responseStarted}
               error={error}
               currentTool={currentTool}
-            />
+            /> */}
           </ScrollShadow>
           <div className="mt-auto flex max-w-full flex-col gap-2">
-            <PromptArea
+            {/* <PromptArea
               disabled={isLoading}
               input={input}
               handleSubmit={handleFormSubmit}
@@ -108,7 +108,7 @@ export default function AIHelper({ onClose }: AIHelperProps) {
                   setInput(e.target.value);
                 }
               }}
-            />
+            /> */}
             <p className="px-2 text-tiny text-default-400">
               AI can make mistakes. Consider checking important information.
             </p>
