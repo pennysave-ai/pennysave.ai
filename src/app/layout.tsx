7fd23d43a6d2a2983b83a3161ae6ac001f175b0b
Mike <mike.zhylevych@gmail.com>
2025-06-08 21:50:53 +0200

    Testemonials images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default async function RootLayout({
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, shrink-to-fit=no"
         />
       </head>
       <Script
