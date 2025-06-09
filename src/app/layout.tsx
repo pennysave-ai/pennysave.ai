da51c5cf9b0b5f9be6ebd1d0cd17e837713eb1df
Mike <mike.zhylevych@gmail.com>
2025-06-09 22:55:51 +0200

    see what is going on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default async function RootLayout({
             gtag('config', 'AW-17082312814');
           `}
       </Script>
-      <Script>
+      <Script id="clarity-init" strategy="afterInteractive">
         {`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
