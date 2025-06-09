a74b4c6909ea00731605ee993e8d380bfab0f188
Mike <mike.zhylevych@gmail.com>
2025-06-09 22:50:39 +0200

    see what is going on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,14 @@ export default async function RootLayout({
             gtag('config', 'AW-17082312814');
           `}
       </Script>
+      <Script>
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rwywkz1jir");
+        `}
+      </Script>
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
