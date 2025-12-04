840c868118c3c3d218f95cc8b43dc1a9a1f21783
Mike <mike.zhylevych@gmail.com>
2025-12-04 12:56:03 +0100

    added redis support

--- next.config.js
+++ next.config.js
@@ -1,18 +1,29 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Forcefully disable reactStrictMode on development
-  // to prevent UseEffect running twice which closing websocket connection
-  reactStrictMode: process.env.NODE_ENV !== "development",
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "redis"],
+  },
 };
 
-// Make sure adding Sentry options is the last code to run before exporting
-module.exports = withSentryConfig(nextConfig, {
-  org: "pennysave",
-  project: "app",
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    // Sentry webpack plugin options
+    silent: true,
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+  },
+  {
+    // Sentry SDK options
+    widenClientFileUpload: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
 
-  // An auth token is required for uploading source maps.
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-
-  silent: false, // Can be used to suppress logs
-});
+    // ✅ Skip source map upload during build (prevents timeout)
+    disableSourceMapUpload: process.env.VERCEL_ENV === "production",
+  }
+);
