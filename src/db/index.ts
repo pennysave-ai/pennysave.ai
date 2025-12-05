0136d9228209e860bd2821140396ae775e6ee65e
Mike <mike.zhylevych@gmail.com>
2025-12-05 23:26:56 +0100

    fixed next vulnarability

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,9 +6,9 @@ declare global {
 }
 
 export const db =
-  global.prisma ||
+  globalThis.prisma ||
   new PrismaClient({
     log: ["query"],
   });
 
-if (process.env.NODE_ENV !== "production") global.prisma = db;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
